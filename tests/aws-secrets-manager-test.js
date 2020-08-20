22bca9758f363cf01d7f2a013af544299dd91444
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-08-20 17:55:07 -0300

    Package base added

---
+++ tests/aws-secrets-manager-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+
+const sinon = require('sinon');
+
+const {
+	AwsSecretsManager,
+	AwsSecretsManagerError
+} = require('../lib');
+
+describe('AwsSecretsManager', () => {
+
+	// your tests here...
+
+});
